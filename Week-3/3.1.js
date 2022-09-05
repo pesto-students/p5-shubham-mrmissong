796b86f3c48e418e5e9d8060b59b55528ab1984b
mrmissong <mrmissong@gmail.com>
2022-09-06 01:33:31 +0530

    Asiignments for Week 3

---
+++ Week-3/3.1.js
@@ -0,0 +1,39 @@
+let total;
+const addMarks = (...args) => {//use of arrow function
+return args.reduce(function (theory, practical) {
+    total = theory + practical
+    return total
+},0)
+}
+
+const memoize = (fun) => { //memoize function
+    let cache =[]
+    return function(...args){
+        let marks = args[0]
+        if (marks in cache) {
+            return cache[marks]
+        }
+        else{
+            let result = args.reduce((theory, practical)=>fun(theory,practical),0)
+            cache[marks]= result;
+            return result
+    }
+}
+}
+const memoized = memoize(addMarks)
+//time and timeEnd to analyze how fast the output is fetched the second time, i.e., from the cache
+console.time() 
+console.log(memoized(40, 60))
+console.timeEnd()
+
+console.time()
+console.log(memoized(40, 60))
+console.timeEnd()
+
+console.time()
+console.log(memoized(30, 44))
+console.timeEnd()
+
+console.time()
+console.log(memoized(30, 44))
+console.timeEnd()
---
+++ Week-3/3.4.js
@@ -0,0 +1,21 @@
+function createStack() {
+  let items = []; // declared item using let
+
+  function push(item) {
+    this.items.push(item);
+  }
+
+  function pop(item) {
+    return this.items.pop();
+  }
+
+  return [push, pop];
+}
+
+const stack = createStack();
+stack.push(10);
+stack.push(5);
+stack.pop();
+stack.items;
+
+console.log(stack.items); // => undefined
