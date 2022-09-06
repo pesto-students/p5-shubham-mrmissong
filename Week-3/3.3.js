99358a7991a78704b5128907a5ca1824baaf85a4
mrmissong <mrmissong@gmail.com>
2022-09-06 12:12:53 +0530

    output problem

--- Week-3/3.3.js
+++ Week-3/3.3.js
@@ -1,17 +1,28 @@
 function createIncrement() {
-    let count=0;
+    let count = 0;
     function increment() {
       count++;
     }
     let message=`Count is ${count}`;
     function log() {
-    console.log(message);
-    //console.log(count);
+      console.log(message);
     }
     return[increment,log];
   }
+// let message;
+//     function increment() {
+//       count++;
+//       message=`Count is ${count}`;
+//     }
+//     function log() {
+//       console.log(message);
+//     }
+//     return[increment,log];
+//   }
   const[increment,log] =createIncrement();
   increment();
   increment();
   increment();
   log();// => Count is 0
+
+  //the output is "Count is 0". the increment changes the count but not the message. the commented code above shows the correction and the message is incremented with every execution.
