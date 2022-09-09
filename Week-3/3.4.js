c7f8afd743ba98827db27d9344b424a40f9ebbbc
Naba Kr Missong <mrmissong@gmail.com>
2022-09-10 01:48:16 +0530

    3.4 update

--- Week-3/3.4.js
+++ Week-3/3.4.js
@@ -1,21 +1,21 @@
 function createStack() {
-  let items = []; // declared item using let
-  function push(item) {
-    this.items.push(item);
-  }
-
-  function pop(item) {
-    return this.items.pop();
-  }
-
-  return [push, pop];
+  // Write your code here...
+  let items = [];
+  return {
+    push: (item) => {
+      items.push(item);
+      console.log(item);
+    },
+    pop: () => {
+      console.log(items.pop());
+      return;
+    }
+  };
 }
-
 const stack = createStack();
-
-stack.push(10);
-stack.push(5);
+stack.push(10); //10
+stack.push(100); // 100
+stack.push(1000); // 1000
 stack.pop();
-stack.items;
 
-console.log(stack.items); // => undefined
+console.log(stack.items); // undefined
