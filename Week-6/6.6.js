a4618133094829a28c8617759e81e5b322cd855f
Naba Kr Missong <mrmissong@gmail.com>
2022-09-27 03:38:25 +0530

    6.6 added

--- Week-6/6.6.js
+++ Week-6/6.6.js
@@ -1,1 +1,30 @@
 //threeSum
+let arr = new Array(-1, 2, 1, 4);
+
+
+
+
+
+
+let closestSum = function(arr, target) {
+    arr.sort((a, b) => a - b);
+    let res = arr[0] + arr[1] + arr[2];
+    for (let i = 0; i < arr.length - 2; i += 1) {
+        let start = i + 1;
+        let end = arr.length - 1;
+        while (start < end) {
+            const sum = arr[i] + arr[start] + arr[end];
+            if (sum > target) {
+                end -= 1;
+            } else {
+                start += 1;
+            }
+             if (Math.abs(target - sum) < Math.abs(target - res)) {
+            res = sum;
+        }
+        }
+    }
+    return res;
+};
+
+console.log(closestSum(arr));
