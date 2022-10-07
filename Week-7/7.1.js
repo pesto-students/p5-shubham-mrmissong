096ce1d2632c43a7a2b3cce6941028378ea76cbe
Naba Kr Missong <mrmissong@gmail.com>
2022-10-08 01:52:07 +0530

    seventh week

---
+++ Week-7/7.1.js
@@ -0,0 +1,66 @@
+class Node {
+	constructor(element) {
+		this.element = element;
+		this.next = null;
+	}
+}
+
+class LinkedList {
+	constructor(value) {
+		this.head = new Node(value);
+		this.size = 1;
+	}
+	length() {
+		return this.size;
+	}
+	append(element) {
+		let node = new Node(element);
+
+		let current;
+
+		if (this.head === null) this.head = node;
+		else {
+			current = this.head;
+
+			while (current.next) {
+				current = current.next;
+			}
+
+			current.next = node;
+		}
+		this.size++;
+	}
+	printList() {
+		let arr = ["head"];
+		let current = this.head;
+		while (current !== null) {
+			console.log(current.element);
+			arr.push(current.element);
+			current = current.next;
+		}
+		arr.push("null");
+		// arr.join("<-->");
+		console.log(arr.join("<-->"));
+		return;
+	}
+	reverse() {
+		let first = this.head;
+		let second = first.next;
+		while (second) {
+			let temp = second.next;
+			second.next = first;
+			first = second;
+			second = temp;
+		}
+		this.head.next = null;
+		this.head = first;
+	}
+}
+
+const ll = new LinkedList("A");
+ll.append("B");
+ll.append("C");
+ll.append("D");
+ll.printList();
+ll.reverse();
+ll.printList();
---
+++ Week-7/7.3.js
@@ -0,0 +1,11 @@
+detectLoop() {
+	if (this.head === null || this.head.next === null) return false;
+	let slow = this.head;
+	let fast = this.head;
+	while (fast.next != null && fast.next.next != null) {
+		fast = fast.next.next;
+		slow = slow.next;
+		if (fast === slow) return true;
+	}
+	return false;
+}
