6d1b741964efbc9df448d7bee2f40ebeaadc7e89
Naba Kr Missong <mrmissong@gmail.com>
2022-10-14 15:03:22 +0530

    buy and sell stocks

--- Week-6/6.4.js
+++ Week-6/6.4.js
@@ -1,18 +1,16 @@
-let prices = new Array(7, 1, 5, 3, 6, 4)
-let profit = 0;
-
-for (let i = 0; i < prices.length; i++) {
-    let base = prices[i];
-    for (let j = i + 1; j < prices.length; j++) {
-        let current = prices[j];
-        if (base > current) {
-            console.log("loss")
-        } else {
-            let diff = current - base;
-            if (diff > profit) {
-                profit = diff;
-            }
-        }
-    }
-}
-console.log(profit);
+let maxProfit = (prices) => {
+	let profit = 0;
+	let minimumPrice = prices[0];
+	for (let i = 0; i < prices.length; i++) {
+		let sp = prices[i];
+		let gain = sp - minimumPrice;
+		if (sp < minimumPrice) {
+			minimumPrice = sp;
+		}
+		if (gain > profit) {
+			profit = gain;
+		}
+	}
+	return profit;
+};
+console.log(maxProfit([7, 1, 5, 3, 6, 4]));
