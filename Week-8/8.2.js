5a59a3a71a96a1cdf7862f06988165152afa3d1a
Naba Kr Missong <mrmissong@gmail.com>
2022-10-30 03:38:22 +0530

    8

---
+++ Week-8/8.2.js
@@ -0,0 +1,12 @@
+var isValidBST = function (root) {
+	return dfs(root, -Infinity, Infinity);
+};
+function dfs(node, min, max) {
+	if (!node) {
+		return true;
+	}
+	if (node.val <= min || node.val >= max) {
+		return false;
+	}
+	return dfs(node.left, min, node.value) && dfs(node.right, node.value, max);
+}
