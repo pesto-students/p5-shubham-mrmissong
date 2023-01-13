13906508ed61f9cc6fc270af037a591edc56cff0
Naba Kr Missong <mrmissong@gmail.com>
2023-01-13 15:21:00 +0530

    server.js

--- Week-14/WealthApp/server.js
+++ Week-14/WealthApp/server.js
@@ -1,42 +1,24 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const User = require("./user");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const authRoute = require("./routes/authRoute");
 
-const app = express();
 const dbURI =
-	"mongodb+srv://vscodemissong:mortyparty@mancode.ku2ubmu.mongodb.net/?retryWrites=true&w=majority";
+	"mongodb+srv://vscodemissong:mortyparty@mancode.ku2ubmu.mongodb.net/testdb?retryWrites=true&w=majority";
 mongoose.set("strictQuery", true);
 mongoose
 	.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-	.then((result) =>
-		app.listen(5000, () => {
-			console.log("i'm running");
-		})
-	)
-	.catch((err) => {
-		console.log(err);
+	.then((res) => {
+		app.listen(3000, () => {
+			console.log("started");
+		});
 	});
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-app.get("/users", (req, res) => {
-	User.find()
-		.then((data) => {
-			console.log(data);
-			res.json(data);
-		})
-		.catch((error) => {
-			console.log(error);
-		});
-});
-app.get("/users/:id", (req, res) => {
-	const id = req.params.id;
-	User.findById(id)
-		.then((data) => {
-			res.json(data);
-		})
-		.catch((error) => {
-			console.log(error);
-		});
-});
+const app = express();
+
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use("/api", authRoute);
